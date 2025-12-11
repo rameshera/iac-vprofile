a611b334e0eee7656b99e642dccf6a40033e019e
rameshera <adminrameshe@gmail.com>
2025-12-11 23:27:10 +0530

    test3

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,22 +2,26 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.25.0"
+      # version = "~> 5.25.0"
+      version = "~> 6.26.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5.1"
+      # version = "~> 3.5.1"
+      version = "~> 3.7.2"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0.4"
+      # version = "~> 4.0.4"
+      version = "~> 4.1.0"
     }
 
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "~> 2.3.2"
+      # version = "~> 2.3.2"
+      version = "~> 2.3.7"
     }
 
     kubernetes = {
