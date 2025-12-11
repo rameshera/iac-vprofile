228e1e8b3ca11472f01de1cd8aa7d369e9d2571a
rameshera <adminrameshe@gmail.com>
2025-12-11 23:49:56 +0530

    test4

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -8,20 +8,20 @@ terraform {
 
     random = {
       source  = "hashicorp/random"
-      # version = "~> 3.5.1"
-      version = "~> 3.7.2"
+      version = "~> 3.5.1"
+      # version = "~> 3.7.2"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      # version = "~> 4.0.4"
-      version = "~> 4.1.0"
+      version = "~> 4.0.4"
+      # version = "~> 4.1.0"
     }
 
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      # version = "~> 2.3.2"
-      version = "~> 2.3.7"
+      version = "~> 2.3.2"
+      # version = "~> 2.3.7"
     }
 
     kubernetes = {
